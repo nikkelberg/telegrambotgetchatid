2e2b64e3b7dca37a6da3726366cf8983869d753f
nicoberg <40277550+nikkelberg@users.noreply.github.com>
2020-02-23 12:06:19 +0100

    Added a table print formatted
    In this versione we can print data in a table

--- chatidtoken.php
+++ chatidtoken.php
@@ -1,6 +1,6 @@
-<?php 
-
-$response = file_get_contents("https://api.telegram.org/bot<BOTTOKEN>/getUpdates");
+Added a table print formatted<?php 
+include 'funzioniutili.php';
+$response = file_get_contents("https://api.telegram.org/bot<bottoken>/getUpdates");
 
 $dati = explode('"id":', $response);//divide for id
 unset($dati[0]);    //delete first array field
@@ -28,19 +28,24 @@ foreach($dati as $value){
 
     array_push($users, $userapp);
 }
+//dd($users);
 
+echo "<table border=\"1\" style=\"width:50%\">";
+echo "<tr><th>CHATID</th><th>NAME</th><th>USERNAME</th></tr>";
 $lastuser = " ";
 foreach($users as $user){
+    echo "<tr>";
     if($user[1] == NULL){ //delete NULL value, needless
         unset($users[$user]); 
     }else{
         if($user[0] != $lastuser){ //control for not repeat value
-            echo $user[0]." ".$user[1]." ".$user[2];
-            echo "<br>";
+            echo "<td>".$user[0]."</td><td>".$user[1]."</td><td>".$user[2]."</td>";
+          //  echo "<br>";
             $lastuser = $user[0];
         }
-    }
-    
+    }    
+    echo "</tr>";
 }
+echo "</table>";
 
 ?>
