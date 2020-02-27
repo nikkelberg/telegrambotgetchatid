6404fcce58c8f17a457bb9a0c1237ae1c71d4875
nicoberg <40277550+nikkelberg@users.noreply.github.com>
2020-02-27 15:22:32 +0100

    Create funzioniutili.php

---
+++ funzioniutili.php
@@ -0,0 +1,9 @@
+<?php
+//questa serve come var_dump ordinata
+function dd($data){
+    highlight_string("<?php\n " . var_export($data, true) . "?>");
+    echo '<script>document.getElementsByTagName("code")[0].getElementsByTagName("span")[1].remove() ;document.getElementsByTagName("code")[0].getElementsByTagName("span")[document.getElementsByTagName("code")[0].getElementsByTagName("span").length - 1].remove() ; </script>';
+    die();
+}
+
+?>
